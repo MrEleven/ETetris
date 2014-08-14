47715d5bc61474c154055b0de699df71100f7e72
yumi <yumi@meishixing.com>
2014-08-15 00:08:57 +0800

    continue, it can be used now

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include "stdlib.h"
 #include "string.h"
 #include "ncurses.h"
+#include "fangkuai.h"
+#include "signal.h"
+#include "unistd.h"
 
 #define PLACE_LINES 21
 #define PLACE_COLS 12
@@ -35,50 +38,6 @@ int place_buf[PLACE_LINES][PLACE_COLS] = {
 
 int current[4][4] = {};
 int next[4][4] = {};
-int fangkuai[7][4][4] = {
-    {
-        {0, 0, 0, 0},
-        {1, 1, 1, 1},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-    },
-    {
-        {0, 0, 0, 0},
-        {1, 1, 0, 0},
-        {0, 1, 1, 0},
-        {0, 0, 0, 0},
-    },
-    {
-        {0, 1, 1, 0},
-        {0, 0, 1, 0},
-        {0, 0, 1, 0},
-        {0, 0, 0, 0},
-    },
-    {
-        {0, 0, 0, 0},
-        {1, 1, 1, 0},
-        {0, 1, 0, 0},
-        {0, 0, 0, 0},
-    },
-    {   
-        {0, 0, 0, 0},
-        {0, 1, 1, 0},
-        {0, 1, 1, 0},
-        {0, 0, 0, 0},
-    },
-    {
-        {0, 1, 1, 0},
-        {0, 1, 0, 0},
-        {0, 1, 0, 0},
-        {0, 0, 0, 0},
-    },
-    {
-        {0, 0, 0, 0},
-        {0, 1, 1, 0},
-        {1, 1, 0, 0},
-        {0, 0, 0, 0},
-    },
-};
 
 typedef struct _POSITION 
 {
@@ -87,22 +46,34 @@ typedef struct _POSITION
 } POSITION;
 
 POSITION current_position;
-int (*current_fangkuai)[4];
+
+int current_status = 0;
+int (*current_fangkuai)[4][4];
 
 void init_place();
 WINDOW *create_newwin(int height, int width, int starty, int startx);
 void paint_win(WINDOW *win, int color);
 void paint_fangkuai(int (*fk)[4], int y, int x, int color);
-void transform(int (*fangkuai)[4]);
+void transform();
 int check_move(int direction);
 int move_fangkuai(int direction);
 int check_transform(int (*fangkuai)[4]);
 
+static void fall_down()
+{
+    if(move_fangkuai(1) == 0)
+    {
+        persistence();
+        cut_the_floor();
+        load_new_fangkuai();
+    }
+    alarm(1);
+}
 
 int main(int argc, char *argv[])
 {   
-    current_position.x = 1;
-    current_position.y = 1;
+    current_position.x = 5;
+    current_position.y = 0;
     WINDOW *my_win;
     int startx, starty, width, height;
     int ch;
@@ -117,9 +88,14 @@ int main(int argc, char *argv[])
     refresh();
     init_place();
 
-    current_fangkuai = fangkuai[2];
-    paint_fangkuai(current_fangkuai, current_position.y, current_position.x, 2);
+    //current_fangkuai = fangkuai[2];
+    load_new_fangkuai();
+    //paint_fangkuai(current_fangkuai[current_status], current_position.y, current_position.x, 2);
     //paint_current(1);
+    
+    signal(SIGALRM, fall_down);
+    alarm(1);
+
     while((ch = getch()) != KEY_F(1))
     {
         switch(ch)
@@ -131,9 +107,9 @@ int main(int argc, char *argv[])
                 move_fangkuai(3);
                 break;
             case KEY_UP:
-                paint_fangkuai(current_fangkuai, current_position.y, current_position.x, 1);
-                transform(current_fangkuai);
-                paint_fangkuai(current_fangkuai, current_position.y, current_position.x, 2);
+                paint_fangkuai(current_fangkuai[current_status], current_position.y, current_position.x, 1);
+                transform();
+                paint_fangkuai(current_fangkuai[current_status], current_position.y, current_position.x, 2);
                 break;
             case KEY_DOWN:
                 move_fangkuai(1);
@@ -162,6 +138,18 @@ void init_place()
             place[y][x] = create_newwin(BLOCK_HEIGHT, BLOCK_WIDTH, starty, startx);
             int color = place_buf[y][x] + 1;
             paint_win(place[y][x], color);
+        }
+    }
+}
+
+void paint_place()
+{
+    int i=0,j=0;
+    for(i=0; i<PLACE_LINES; i++)
+    {
+        for(j=0; j<PLACE_COLS; j++)
+        {
+            paint_win(place[i][j], place_buf[i][j] + 1);
         }
     }
 }
@@ -201,30 +189,15 @@ WINDOW *create_newwin(int height, int width, int starty, int startx)
     return local_win;
 }
 
-void transform(int (*fangkuai)[4])
+void transform()
 {
     int x=0, y=0;
-    if(check_transform(fangkuai) == 0)
+    if(check_transform(current_fangkuai[current_status]) == 0)
     {
         return;
     }
-    
-    for(x=0;x<4;x++)
-    {
-        for(y=0;y<4;y++)
-        {
-            next[x][y] = fangkuai[x][y];
-        }
-    }
-
-    for(y=0; y<4; y++)
-    {
-        for(x=0; x<4; x++)
-        {
-            current[3-x][y] = next[y][x];
-        }
-    }
-    current_fangkuai = current;
+    current_status ++;
+    current_status = current_status % 4;
 }
 
 int check_transform(int (*fangkuai)[4])
@@ -254,7 +227,7 @@ int check_move(int direction)
     {
         for(j=0; j<4;j++)
         {
-            if(current_fangkuai[i][j] == 1)
+            if(current_fangkuai[current_status][i][j] == 1)
             {
                 if(direction == 1)
                 {
@@ -287,7 +260,7 @@ int move_fangkuai(int direction)
 {
     if(check_move(direction))
     {
-        paint_fangkuai(current_fangkuai, current_position.y, current_position.x, 1);
+        paint_fangkuai(current_fangkuai[current_status], current_position.y, current_position.x, 1);
         if(direction == 1)
         {
             current_position.y++;
@@ -300,6 +273,71 @@ int move_fangkuai(int direction)
         {
             current_position.x++;
         }
-        paint_fangkuai(current_fangkuai, current_position.y, current_position.x, 2);
+        paint_fangkuai(current_fangkuai[current_status], current_position.y, current_position.x, 2);
+        return 1;
     }
+    return 0;
+}
+
+int persistence()
+{
+    int i =0, j =0;
+    for(i=0; i<4; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            place_buf[current_position.y + i][current_position.x+j] += current_fangkuai[current_status][i][j];
+        }
+    }
+    return 1;
+}
+
+int load_new_fangkuai()
+{
+    current_position.y = 0;
+    current_position.x = 5;
+    current_status = 0;
+    int f = rand() % 7;
+    current_fangkuai = fangkuai[f];
+    paint_fangkuai(current_fangkuai[current_status], current_position.y, current_position.x, 2);
+}
+
+int cut_the_floor()
+{
+    int i = PLACE_COLS;
+    int l = 0;
+    int change = 0;
+    for(l=current_position.y; l<current_position.y + 4 && l < PLACE_LINES-1; l++)
+    {
+        int full = 1;
+        for(i = 1; i < PLACE_COLS-1; i++)
+        {
+            if(place_buf[l][i] == 0)
+            {
+                full = 0;
+                break;
+            }
+        }
+        if(full)
+        {
+            change = 1;
+            int o=0, p=0;
+            for(o=l; o>0; o-- )
+            {
+                for(p = 1; p < PLACE_COLS - 1; p++)
+                {
+                    place_buf[o][p] = place_buf[o-1][p];
+                }
+            }
+            for(p=1; p<PLACE_COLS -1; p++)
+            {
+                place_buf[0][p] = 0;
+            }
+        }
+    }
+    if(change)
+    {
+        paint_place();
+    }
+    return 1;
 }
